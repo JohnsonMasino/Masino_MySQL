dfa009267f71cfa2cac7c8bd37eb6764a720a599
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-21 10:29:36 -0700

    Learniing MySQL

--- file_0.sql
+++ file_0.sql
@@ -9,3 +9,8 @@ CREATE TABLE customer
 	state VARCHAR(15) DEFAULT "State" NOT NULL,
 	age INT(3) NOT NULL DEFAULT '001',
 );
+
+SELECT gender, net_worth, state
+FROM customer
+WHERE age > 18 AND age < 60
+ORDER BY state, gender, net_worth;
