b9f2ff7bc8162f838f86d3e64bce0e084e4954bd
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-23 08:20:38 -0700

    Apache

---
+++ Puppet_Codes/file0.pp
@@ -0,0 +1,11 @@
+class nginx {
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  service { 'apache2':
+    ensure  => running,
+    enable  => true,
+    require => Package['apache2'],
+  }
+}
